782aa7c2c4fa9b6365ec967e88205fe4232342da
Валентин Родионович Илларионов <i-valentin@mail.ru>
2022-09-20 00:49:18 +0900

    Выполнено 1 задание

---
+++ HomeWork1/HomeWork1/PostModel.cs
@@ -0,0 +1,15 @@
+﻿namespace HomeWork1
+{
+    public class PostModel
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+        public override string ToString()
+        {
+            return $" {UserId}\n {Id}\n {Title}\n {Body = Body.Replace("\n", @"\n")}\n";
+        }
+    }
+}
---
+++ HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    internal class Program
+    {
+        private static HttpClient client = new HttpClient();
+        private static readonly CancellationToken cts = new CancellationToken();
+        private static readonly string path = "result.txt";
+
+        static async Task Main(string[] args)
+        {
+            await GetPosts(cts);
+        }
+
+        public static async Task GetPosts(CancellationToken cancelationToken)
+        {
+
+            for (int i = 1; i < 11; i++)
+            {
+                var response = await client.GetAsync($"https://jsonplaceholder.typicode.com/posts/{i}", cancelationToken);
+                var jsonModel = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<PostModel>(jsonModel);
+                await SavePost(result);
+                Console.WriteLine("Все сохранено");
+            }
+
+        }
+
+        public static async Task SavePost(PostModel model)
+        {
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                await writer.WriteLineAsync(model.ToString());
+            }
+        }
+    }
+}
